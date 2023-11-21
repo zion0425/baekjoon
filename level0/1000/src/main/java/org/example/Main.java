d608933f938da15ed88e8d8252004f5e2b3a5214
zion0425 <zion0425@naver.com>
2023-11-21 17:43:48 +0900

    feat(A+B): BuffredReader, StringTokenizer 사용

---
+++ level0/1000/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Integer sum = 0;
+
+        while (st.hasMoreTokens()) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+        System.out.println(sum);
+    }
+}
