34260d91672f5f6f979547255e7fdff926ab75e9
zion0425 <zion0425@naver.com>
2023-11-23 00:18:12 +0900

    feat: isChecked로 bool배열로 범위 안에 빈 값 찾기

--- level0/1000/src/main/java/org/example/S5597.java
+++ level0/1000/src/main/java/org/example/S5597.java
@@ -1,2 +1,26 @@
-package org.example;public class S5597 {
+package org.example;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class S5597 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Boolean[] checked = new Boolean[31];
+        Arrays.fill(checked, false);
+        for (int i = 1; i <= 28; i++) {
+            checked[Integer.parseInt(br.readLine())] = true;
+        }
+
+        for (int j = 1; j < checked.length; j++) {
+            if (!checked[j]) {
+                bw.write(j + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
