2caf577c73ada435bab9c8689f1ff9800f5cb527
zion0425 <zion0425@naver.com>
2023-11-23 00:20:01 +0900

    feat(10807): stream, count로 특정 문자 개수 찾기

--- level0/1000/src/main/java/org/stream/S10807.java
+++ level0/1000/src/main/java/org/stream/S10807.java
@@ -1,2 +1,25 @@
-package org.stream;public class S10807 {
+package org.stream;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class S10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine();
+        String inputs = br.readLine();
+        String inputNumbers[] = inputs.split(" ");
+        String matchCharacter = br.readLine();
+
+        long count = Arrays.stream(inputNumbers)
+                .filter(s -> s.equals(matchCharacter))
+                .count();
+
+        bw.write(count + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
