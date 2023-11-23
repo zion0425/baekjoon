297219248ae3e05427f0577767f925fb485b66e2
Kimsion <30633116+zion0425@users.noreply.github.com>
2023-11-23 12:23:09 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -6,13 +6,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
         boolean[][] whiteSection = new boolean[101][101];
         for (boolean[] a : whiteSection) {
             Arrays.fill(a, false);
         }
 
-        StringTokenizer st;
+        int cnt = 0;
         Integer paperCount = Integer.parseInt(br.readLine());
         while (paperCount-- > 0) {
             st = new StringTokenizer(br.readLine());
@@ -20,16 +21,11 @@ public class Main {
             int input2 = Integer.parseInt(st.nextToken());
             for (int i = input1; i < input1 + 10; i++) {
                 for (int j = input2; j < input2 + 10; j++) {
-                    whiteSection[i][j] = true;
+                    if (!whiteSection[i][j]) {
+                        cnt++;
+                        whiteSection[i][j] = true;
+                    }
                 }
-            }
-        }
-
-        int cnt = 0 ;
-        for (int i = 0 ; i < 100; i++){
-            for (int j = 0; j < 100; j ++) {
-                if (whiteSection[i][j] == true)
-                    cnt ++;
             }
         }
 
