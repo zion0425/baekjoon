e6cafd6ad0b546dbf02ca47ad054a3c407dd65c8
zion0425 <zion0425@naver.com>
2023-11-24 01:37:48 +0900

    feat(1436): brute force

---
+++ level0/1000/src/main/java/org/bruteforce/S1436.java
@@ -0,0 +1,24 @@
+package org.bruteforce;
+
+import java.io.*;
+
+public class S1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Integer num = Integer.parseInt(br.readLine());
+        Integer resNum = 0;
+        int cnt = 0;
+        for (Integer i = 666; cnt < num; i++) {
+            if (String.valueOf(i).contains("666")) {
+                cnt++;
+                resNum = i;
+            }
+        }
+        bw.write(resNum + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
