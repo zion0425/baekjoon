5bd2a02e50f28899ef75d1fb048f91a06db31333
Kimsion <30633116+zion0425@users.noreply.github.com>
2023-11-23 19:05:39 +0900

    [Bronze I] Title: 소인수분해, Time: 192 ms, Memory: 16100 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Integer number = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                bw.write(i + "\n");
+                number /= i;
+                i = 1;
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
