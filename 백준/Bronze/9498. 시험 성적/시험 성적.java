df4e156d0fbbc8ce3cb9b7641093e42e003aafc5
Kimsion <30633116+zion0425@users.noreply.github.com>
2023-11-26 23:11:22 +0900

    [Bronze V] Title: 시험 성적, Time: 132 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Integer grade = Integer.parseInt(st.nextToken());
+        if (grade >= 90) {
+            System.out.println("A");
+        } else if (grade >= 80) {
+            System.out.println("B");
+        } else if (grade >= 70) {
+            System.out.println("C");
+        } else if (grade >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
