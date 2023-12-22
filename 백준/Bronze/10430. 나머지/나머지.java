de085bb864d28ba8d840c9917e827906f20963d7
Kimsion <30633116+zion0425@users.noreply.github.com>
2023-12-22 16:22:41 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String nums = br.readLine();
+        StringTokenizer st = new StringTokenizer(nums);
+        Integer A = Integer.parseInt(st.nextToken());
+        Integer B = Integer.parseInt(st.nextToken());
+        Integer C = Integer.parseInt(st.nextToken());
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
+    }
+}
