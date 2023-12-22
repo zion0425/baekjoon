f52ff288de7d5f96c393de65d936331af172e360
Kimsion <30633116+zion0425@users.noreply.github.com>
2023-12-22 16:03:27 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String nums = br.readLine();
+        StringTokenizer st = new StringTokenizer(nums);
+        Integer year = Integer.parseInt(st.nextToken());
+        System.out.println(year - (2541 - 1998));
+    }
+}
