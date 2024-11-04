9de7ff7db7c26e9530adb44040f42303c9a2fb76
Kimsion <30633116+zion0425@users.noreply.github.com>
2024-11-04 21:25:43 +0900

    [Silver IV] Title: 스택, Time: 168 ms, Memory: 18856 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -6,36 +6,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
         int n = Integer.parseInt(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
-
-        StringTokenizer st;
         while (n-- > 0) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
             switch (command) {
-                case "push" :
-                    int value = Integer.parseInt(st.nextToken());
-                    stack.push(value);
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
                     break;
-                case "top" :
-                    if (stack.isEmpty()) sb.append("-1");
-                    else sb.append(stack.peek());
-                    sb.append('\n');
+                case "pop":
+                    sb.append(stack.isEmpty() ? "-1" : stack.pop()).append('\n');
                     break;
-                case "size" :
+                case "top":
+                    sb.append(stack.isEmpty() ? "-1" : stack.peek()).append('\n');
+                    break;
+                case "size":
                     sb.append(stack.size()).append('\n');
                     break;
-                case "empty" :
-                    if (stack.isEmpty()) sb.append('1');
-                    else sb.append('0');
-                    sb.append('\n');
-                    break;
-                case "pop" :
-                    if (stack.isEmpty()) sb.append("-1");
-                    else sb.append(stack.pop());
-                    sb.append('\n');
+                case "empty":
+                    sb.append(stack.isEmpty() ? '1' : '0').append('\n');
                     break;
             }
         }
