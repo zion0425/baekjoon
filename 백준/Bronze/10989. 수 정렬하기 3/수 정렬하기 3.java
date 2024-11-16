bfc89957042822ee2e3a3bb069bdecd12ab6bb29
Kimsion <30633116+zion0425@users.noreply.github.com>
2024-11-16 20:10:28 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2372 ms, Memory: 361680 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        for (int i =0; i < n; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
